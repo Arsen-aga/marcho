c35e78ac07c2dc8d5c4ad7f4685228ad9969124e
Asanov Arsen <tatarin08082002@gmail.com>
2024-01-17 17:02:31 +0300

    added blog

--- gulpfile.js
+++ gulpfile.js
@@ -47,15 +47,15 @@ function scripts() {
 function images() {
   return src('app/images/**/*.*')
     .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
+      // imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
+      imagemin.optipng({ optimizationLevel: 10 }),
+      // imagemin.svgo({
+      //   plugins: [
+      //     { removeViewBox: true },
+      //     { cleanupIDs: false }
+      //   ]
+      // })
     ]))
     .pipe(dest('dist/images'))
 }
