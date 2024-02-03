883ad4d3b83cb5dbf9b4ff035102f6a9fc985f2f
Asanov Arsen <tatarin08082002@gmail.com>
2024-02-03 22:49:31 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,34 @@
 $(function () {
-  $('.top-slider__inner').slick({
+  $(".shop-content__filter-btn").on("click", function () {
+    $(".shop-content__filter-btn").removeClass(
+      "shop-content__filter-btn--active"
+    );
+    $(this).addClass("shop-content__filter-btn--active");
+  });
+
+  $(".button-list").on("click", function () {
+    $(".product-item").addClass("product-item--list");
+  });
+  $(".button-grid").on("click", function () {
+    $(".product-item").removeClass("product-item--list");
+  });
+
+  $(".select-style").styler();
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+    onChange: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+  });
+
+  $(".top-slider__inner").slick({
     dots: true,
     arrows: false,
     fade: true,
@@ -11,7 +40,7 @@ $(function () {
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
-    readOnly: true
+    readOnly: true,
   });
 
   function getTimeRemaining(endtime) {
@@ -54,8 +83,6 @@ $(function () {
     const timeinterval = setInterval(updateClock, 1000);
   }
 
-  const deadline = $('.promo__clock').attr('data-time');
+  const deadline = $(".promo__clock").attr("data-time");
   initializeClock(".promo__clock", deadline);
-
-})
-
+});
