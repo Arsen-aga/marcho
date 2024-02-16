75d8adf9c3282683d90912549f2227eefa3a1e46
Arsen Asanov <tatarin08082002@gmail.com>
2024-02-17 02:11:47 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,88 +1,135 @@
 $(function () {
-  $(".shop-content__filter-btn").on("click", function () {
-    $(".shop-content__filter-btn").removeClass(
-      "shop-content__filter-btn--active"
-    );
-    $(this).addClass("shop-content__filter-btn--active");
-  });
+  if ($(".product-tabs__top-item")) {
+    $(".product-tabs__top-item").on("click", function (e) {
+      e.preventDefault();
+      $(".product-tabs__top-item").removeClass(
+        "product-tabs__top-item--active"
+      );
+      $(this).addClass("product-tabs__top-item--active");
 
-  $(".button-list").on("click", function () {
-    $(".product-item").addClass("product-item--list");
-  });
-  $(".button-grid").on("click", function () {
-    $(".product-item").removeClass("product-item--list");
-  });
-
-  $(".select-style").styler();
-
-  $(".filter-price__input").ionRangeSlider({
-    type: "double",
-    prefix: "$",
-    onStart: function (data) {
-      $(".filter-price__from").text(data.from);
-      $(".filter-price__to").text(data.to);
-    },
-    onChange: function (data) {
-      $(".filter-price__from").text(data.from);
-      $(".filter-price__to").text(data.to);
-    },
-  });
-
-  $(".top-slider__inner").slick({
-    dots: true,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
-
-  $(".star").rateYo({
-    starWidth: "17px",
-    normalFill: "#ccccce",
-    ratedFill: "#ffc35b",
-    readOnly: true,
-  });
-
-  function getTimeRemaining(endtime) {
-    const total = Date.parse(endtime) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
-    return {
-      total,
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
+      $(".product-tabs__content-item").removeClass(
+        "product-tabs__content-item--active"
+      );
+      $($(this).attr('href')).addClass("product-tabs__content-item--active");
+    });
   }
 
-  function initializeClock(className, endtime) {
-    const clock = document.querySelector(className);
-    const daysSpan = clock.querySelector(".promo__days");
-    const hoursSpan = clock.querySelector(".promo__hours");
-    const minutesSpan = clock.querySelector(".promo__minutes");
-    const secondsSpan = clock.querySelector(".promo__seconds");
+  if ($(".product-slide__thumb")) {
+    $(".product-slide__thumb").slick({
+      asNavFor: ".product-slide__big",
+      focusOnSelect: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      vertical: true,
+      draggable: false,
+    });
+    $(".product-slide__big").slick({
+      asNavFor: ".product-slide__thumb",
+      draggable: false,
+      arrows: false,
+      fade: true,
+    });
+  }
 
-    function updateClock() {
-      const t = getTimeRemaining(endtime);
+  if ($(".shop-content__filter-btn")) {
+    $(".shop-content__filter-btn").on("click", function () {
+      $(".shop-content__filter-btn").removeClass(
+        "shop-content__filter-btn--active"
+      );
+      $(this).addClass("shop-content__filter-btn--active");
+    });
+  }
 
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+  if ($(".button-list")) {
+    $(".button-list").on("click", function () {
+      $(".product-item").addClass("product-item--list");
+    });
+    $(".button-grid").on("click", function () {
+      $(".product-item, .product-one__num").removeClass("product-item--list");
+    });
+  }
 
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
+  if ($(".select-style") || $(".product-one__num")) {
+    $(".select-style").styler();
+    $(".product-one__num").styler();
+  }
+
+  if ($(".filter-price__input")) {
+    $(".filter-price__input").ionRangeSlider({
+      type: "double",
+      prefix: "$",
+      onStart: function (data) {
+        $(".filter-price__from").text(data.from);
+        $(".filter-price__to").text(data.to);
+      },
+      onChange: function (data) {
+        $(".filter-price__from").text(data.from);
+        $(".filter-price__to").text(data.to);
+      },
+    });
+  }
+
+  if ($(".top-slider__inner")) {
+    $(".top-slider__inner").slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  }
+
+  if ($(".star")) {
+    $(".star").rateYo({
+      starWidth: "17px",
+      normalFill: "#ccccce",
+      ratedFill: "#ffc35b",
+      readOnly: true,
+    });
+  }
+
+  if ($(".promo__clock")) {
+    function getTimeRemaining(endtime) {
+      const total = Date.parse(endtime) - Date.parse(new Date());
+      const seconds = Math.floor((total / 1000) % 60);
+      const minutes = Math.floor((total / 1000 / 60) % 60);
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+      return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds,
+      };
     }
 
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
-  }
+    function initializeClock(className, endtime) {
+      const clock = document.querySelector(className);
+      const daysSpan = clock.querySelector(".promo__days");
+      const hoursSpan = clock.querySelector(".promo__hours");
+      const minutesSpan = clock.querySelector(".promo__minutes");
+      const secondsSpan = clock.querySelector(".promo__seconds");
 
-  const deadline = $(".promo__clock").attr("data-time");
-  initializeClock(".promo__clock", deadline);
+      function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+        minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+      }
+
+      updateClock();
+      const timeinterval = setInterval(updateClock, 1000);
+    }
+
+    const deadline = $(".promo__clock").attr("data-time");
+    initializeClock(".promo__clock", deadline);
+  }
 });
